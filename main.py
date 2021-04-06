109dda7e11f30be397a8630f1d139ac0347c70a5
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-06 05:50:16 -0400

    main scene/object classes

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import os
 import time
+import random
 
-print('test', end='\r')
+class Scene:
+    def __init__(self):
+        self.objects = []
+    def add(self, obj):
+        self.objects.append(obj)
 
-time.sleep(1)
-# os.system('cls')
-print('test2', end='\r')
+class Object:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def info(self):
+        return '\n'.join(str(n) for n in [self.x, self.y])
+
+sim = Scene()
+for i in range(20):
+    x = random.randint(0, 20)
+    y = random.randint(0, 20)
+    sim.add(obj=Object(x, y))
+
+print(sim.objects[0].info())
