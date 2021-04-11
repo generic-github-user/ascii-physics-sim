b2ff6e5dedc73a0b6a4e12775f2f59d3ba0878b8
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-11 17:52:01 -0400

    Material class documentation

--- main.py
+++ main.py
@@ -156,9 +156,14 @@ class PhysProps:
 
 # TODO: diagram (auto?)
 class Material:
+    """A physical material, as described by its properties and appearance"""
     def __init__(self, name, abbr, elasticity, density, color='', opacity=1, ior=0, softness=1, explosiveness=0, volatility=1):
-        self.name = name
-        self.abbr = abbr
+        """Create a new material; this can be reused over multiple objects"""
+
+        self.name: str = name
+        """The name of the material; 'Gold'"""
+        self.abbr: str = abbr
+        """An abbreviated name for the material; 'Au'"""
 
         self.elasticity = elasticity
         self.stiffness = self.elasticity
@@ -172,7 +177,9 @@ class Material:
         self.malleability = self.softness
 
         self.explosiveness = explosiveness
+        """Energy released when the material combusts (based on volatility threshold)"""
         self.volatility = volatility
+        """Sensitivity of material to external forces; if this is exceeded, explosion will occur"""
 
 class Matter:
     def __init__(self, geometry, material):
